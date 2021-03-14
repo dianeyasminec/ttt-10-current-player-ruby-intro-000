e468545d1018ac115d6f89eab70e4a00804cbd78
Diane Compaore <dianeyasminec60@gmail.com>
2021-03-14 22:20:09 +0000

    Automatically backed up by Learn

--- lib/notes.rb
+++ lib/notes.rb
@@ -3,7 +3,8 @@
   print"/"
   puts " Here are the first 100 numbers divisble by 2"
   counter=0
-  until counter==100
+  while counter==100
+    puts counter
   counter=+1
-  puts "#{counter.}"
+
 end
