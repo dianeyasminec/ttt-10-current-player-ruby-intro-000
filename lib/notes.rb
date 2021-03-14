2b8531d3dbcf1f9a5cecfbe14e2f56911ca9b966
Diane Compaore <dianeyasminec60@gmail.com>
2021-03-14 22:13:10 +0000

    Automatically backed up by Learn

---
+++ lib/notes.rb
@@ -0,0 +1,9 @@
+#part 1
+24.times do
+  print"/"
+  puts " Here are the first 100 numbers divisble by 2"
+  counter=0
+  until counter==100
+  counter=+2
+end
+ end
---
+++ lib/turn_count.rb
@@ -0,0 +1,7 @@
+def turn_count
+  board=["X","O"]
+  "X"=token
+  counter = 0
+  if
+    counter = counter + 1
+  end
